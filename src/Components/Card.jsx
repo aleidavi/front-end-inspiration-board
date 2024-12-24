a8975cd76ffb66661310a589c4bb2379f823ac03
mayboolean <>
2024-12-23 22:10:07 -0500

    updates some css

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -2,7 +2,10 @@ import './Card.css'
 
 const Card = ({ id, message, boardId, likesCount}) => {
     return (
-        <div className='card-box'> {message}</div>
+        <div className={`card-box c${boardId}`}> 
+            {message}
+            <button className='plus1'>+1</button>
+        </div>
     )
 }
 export default Card;
