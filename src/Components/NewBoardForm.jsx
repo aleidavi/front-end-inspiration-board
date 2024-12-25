caaa6a61db173ee5e93e793864c7200c69ba7a68
mayboolean <>
2024-12-25 14:14:32 -0500

    handles error if title and/or owner is not filled when submitting

--- src/Components/NewBoardForm.jsx
+++ src/Components/NewBoardForm.jsx
@@ -1,10 +1,13 @@
 import { useState } from "react";
+import './NewBoardForm.css'
+
 const NewBoardForm = ({ handleBoardSubmit}) => {
   const kDefaultBoardForm = {
     title: "",
     owner: "",
   };
   const [boardFormData, setBoardFormData] = useState(kDefaultBoardForm);
+  const [errorMessage, setErrorMessage] = useState("")
 
   const handleChange = (event) => {
     const fieldName = event.target.name;
@@ -16,36 +19,51 @@ const NewBoardForm = ({ handleBoardSubmit}) => {
 
   const onHandleSubmit = (event) => {
     event.preventDefault();
-    handleBoardSubmit(boardFormData);
-    setBoardFormData(kDefaultBoardForm);
+    if (!boardFormData.title && !boardFormData.owner) {
+      setErrorMessage("Error: Title and Owner cannot be empty!")
+    } else if (!boardFormData.title) {
+      setErrorMessage("Title cannot be empty!")
+    } else if (!boardFormData.owner) {
+      setErrorMessage("Owner cannot be empty!")
+    } else {
+      handleBoardSubmit(boardFormData);
+      setBoardFormData(kDefaultBoardForm);
+    }
   }
 
+  const emptyTitle = !boardFormData.title ? 'empty': ''
+  const emptyOwner = !boardFormData.owner ? 'empty': ''
+
   return (
-    <form onSubmit={onHandleSubmit}>
-      <div>
-        <label htmlFor="title">Title: </label>
-        <input
-          type="text"
-          id="title"
-          name="title"
-          value={boardFormData.title}
-          onChange={handleChange}
-        ></input>
-      </div>
-      <div>
-        <label htmlFor="owner">Owner: </label>
-        <input
-          type="text"
-          id="owner"
-          name="owner"
-          value={boardFormData.value}
-          onChange={handleChange}
-        ></input>
-      </div>
-      <div>
-        <input type="submit"></input>
-      </div>
-    </form>
+    <>
+      <form onSubmit={onHandleSubmit}>
+        <div className="form-field">
+          <label htmlFor="title">Title:  </label>
+          <input className={emptyTitle}
+            type="text"
+            id="title"
+            name="title"
+            value={boardFormData.title}
+            onChange={handleChange}
+          ></input>
+        </div>
+        <div className="form-field">
+          <label htmlFor="owner">Owner: </label>
+          <input className={emptyOwner}
+            type="text"
+            id="owner"
+            name="owner"
+            value={boardFormData.value}
+            onChange={handleChange}
+          ></input>
+        </div>
+        <div className='submit-container'>
+          <input className='submit-button'
+          type='submit'></input>
+        </div>
+      </form>
+      <div>{errorMessage}</div>
+    </>
   );
 };
 export default NewBoardForm;
