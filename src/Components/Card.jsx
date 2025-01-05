621a3c704b889dbab438ee6a07ee3276b04ceaea
Aleida Vieyra <avieyra@ufl.edu>
2025-01-05 10:07:47 -0500

    passed down handleDeleteCard and handleLikesCount props to Card component

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,12 +1,22 @@
 import "./Card.css";
 
-const Card = ({ id, message, boardId, likesCount }) => {
-  return (
-    <div className={`card-box c${boardId}`}>
-      <button className="delete">x</button>
-      {message}
-      <button className="plus1">+1</button>
-    </div>
-  );
-};
+const Card = ({ id, message, boardId, likesCount, handleDeleteCard, handleLikesCount }) => {
+
+	const onDeleteCard = () => {
+		handleDeleteCard(id);
+	};
+
+	const onLikeCard = () => {
+		handleLikesCount(id);
+	};
+
+	return (
+		<div className={`card-box c${boardId}`}>
+		<button className="delete" onClick={onDeleteCard}>x</button>
+		{message}
+		<button className="plus1" onClick={onLikeCard}>+1</button>
+		</div>
+	);
+	};
+
 export default Card;
